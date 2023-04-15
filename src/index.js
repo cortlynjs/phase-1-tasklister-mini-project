1278b93a64a45298797eea81c7cc10c7ed0efa46
Cortlyn Stafford <cortlynstafford@icloud.com>
2023-04-15 10:53:49 -0400

    TaskLister

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+const form = document.getElementById("create-task-form");
+
+form.addEventListener("submit", function(event) {
+  event.preventDefault(); // prevent default form submission behavior
+
+  const input = document.getElementById("new-task-description");
+  const task = input.value;
+
+  const taskList = document.getElementById("tasks");
+  const taskItem = document.createElement("li");
+  taskItem.innerText = task;
+  taskList.appendChild(taskItem);
+
+  input.value = ""; // clear input field
 });
+
